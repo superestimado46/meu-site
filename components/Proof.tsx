826fe759f0d73f1d8d6800b5288014a6d2ec3f1b
superestimado46 <130703001+superestimado46@users.noreply.github.com>
2026-03-08 18:59:53 -0300

    Proof.tsx

--- components/Proof.tsx
+++ components/Proof.tsx
@@ -3,20 +3,10 @@ import ResultsMarquee from "./ResultsMarquee";
 import TestimonialsMarquee from "./TestimonialsMarquee";
 
 export default function Proof() {
-return (
-<>
-{/* LINHA 1: Prints de resultados rolando */} <ResultsMarquee />
-
-```
-  {/* Texto entre as duas linhas */}
-  <p className="text-center text-lg opacity-80 my-10">
-    Esses são apenas alguns dos resultados…
-  </p>
-
-  {/* LINHA 2: Depoimentos rolando */}
-  <TestimonialsMarquee />
-</>
-```
-
-);
+  return (
+    <>
+      <ResultsMarquee />
+      <TestimonialsMarquee />
+    </>
+  );
 }
