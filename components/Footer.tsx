b4b14d641214ca32d50507e760ce46fc811eea0b
superestimado46 <130703001+superestimado46@users.noreply.github.com>
2026-03-11 15:15:58 -0300

    Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,12 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-black py-12 border-t border-gray-900">
       <div className="container mx-auto px-4 text-center">
-        <h2 className="text-2xl font-heading font-bold text-white mb-4">SIMPSONS CASH</h2>
+        <h2 className="text-2xl font-heading font-bold text-white mb-4">STORM CLIPS</h2>
         <p className="text-gray-500 text-sm mb-8 max-w-md mx-auto">
           Este site não faz parte do site do TikTok ou da ByteDance Ltd. Além disso, este site NÃO é endossado pelo TikTok de forma alguma.
         </p>
         <p className="text-gray-700 text-xs">
-          &copy; {new Date().getFullYear()} Simpsons Cash. Todos os direitos reservados.
+          &copy; {new Date().getFullYear()} Storm Clips. Todos os direitos reservados.
         </p>
       </div>
     </footer>
